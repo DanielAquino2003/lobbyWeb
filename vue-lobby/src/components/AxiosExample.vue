934b7d8fff4f3d8ff7504b5c0d6155306e92e18a
daniel <aquinosantiagodani@gmail.com>
2024-05-07 00:07:42 +0200

    ejemplo de axio connection to an api

---
+++ vue-lobby/src/components/AxiosExample.vue
@@ -0,0 +1,23 @@
+<template>
+    <div v-if="result" class="content">
+      <p>User ID: {{ result.userId }}</p>
+      <p>Title: {{ result.title }}</p>
+    </div>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  export default {
+    data: () => ({
+      result: null,
+    }),
+    created() {
+      axios.get("https://jsonplaceholder.typicode.com/todos/1").then((result) => {
+        this.result = result.data;
+        console.log(result.data);
+      });
+    },
+  };
+  </script>
+  
+  <style scoped lang="scss"></style>
